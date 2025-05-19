f6b2a8cee0f38e1207711f05a0bb3faad993e1d0
Patrick <patrickrebecchi@hotmail.com>
2025-05-19 14:30:10 -0300

    Corrigindo erros do blazor Web

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -9,7 +9,8 @@ public class ScreenSoundContext : DbContext
     public DbSet<Musica> Musicas { get; set; }
     public DbSet<Genero> Generos { get; set; }
 
-    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=true;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    // banco de casa - private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=true;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
     //private string connectionString = "Server=tcp:screensoundserver0.database.windows.net,1433;Initial Catalog=ScreenSoundV0;Persist Security Info=False;User ID=patrick;Password=Senh@001;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;";
     //public ScreenSoundContext(DbContextOptions<ScreenSoundContext> options) : base(options)
     //{
